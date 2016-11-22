f8e209ab74e54aeaa81d495acebfccb0dbbb3288
HaxorFox <haxorfox@nicafur.com>
2016-11-21 18:06:30 -0600

    Agregando Splash Screen y algunos valores. Eliminando el Splash anterior.

--- app/src/main/java/com/nicarial/nicarial/MenuPrincipal.java
+++ app/src/main/java/com/nicarial/nicarial/MenuPrincipal.java
@@ -2,12 +2,23 @@ package com.nicarial.nicarial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.stephentuso.welcome.WelcomeHelper;
 
 public class MenuPrincipal extends AppCompatActivity {
+    WelcomeHelper welcomeScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        welcomeScreen.onSaveInstanceState(outState);
     }
 }
