574177ac8af339a1cb56a4921c4dec2bd0ef653e
HaxorFox <haxorfox@nicafur.com>
2016-11-22 22:56:38 -0600

    Arreglando titulos y algunas cosas como los llamados.

--- app/src/main/java/com/nicarial/nicarial/MenuPrincipal.java
+++ app/src/main/java/com/nicarial/nicarial/MenuPrincipal.java
@@ -1,24 +1,44 @@
 package com.nicarial.nicarial;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-
-import com.stephentuso.welcome.WelcomeHelper;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MenuPrincipal extends AppCompatActivity {
-    WelcomeHelper welcomeScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-    }
+        ImageView ivuser = (ImageView) findViewById(R.id.ivuser);
+        ivuser.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent1 = new Intent(v.getContext(), Usuarios.class);
+                startActivityForResult(intent1, 0);
+            }
+        });
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        welcomeScreen.onSaveInstanceState(outState);
+        ImageView ivdonation = (ImageView) findViewById(R.id.ivdonation);
+        ivdonation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent2 = new Intent(v.getContext(), Donaciones.class);
+                startActivityForResult(intent2, 0);
+            }
+        });
+
+        ImageView ivcalculator = (ImageView) findViewById(R.id.ivcalculator);
+        ivcalculator.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent3 = new Intent(v.getContext(), Ingresos.class);
+                startActivityForResult(intent3, 0);
+            }
+        });
+
     }
 }
