c83bd6ffc538af9bf0e3a8ddb4612618ff5ee6a3
draenwing <draenwing@gmail.com>
2016-11-27 07:07:04 +0800

    boton añadido
    se agrego el boton acerca, para la info de la app

---
+++ app/src/main/java/com/nicarial/nicarial/Contract.java
@@ -0,0 +1,17 @@
+package com.nicarial.nicarial;
+
+/**
+ * Created by USUARIO on 27/11/2016.
+ */
+
+public class Contract {
+
+    int _id;
+    String _nombre;
+    String _comentario;
+
+    public Contract(){}
+
+
+
+}
