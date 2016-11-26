c02e0132973da2c3ee238ef3aabd6893955fc35e
draenwing <draenwing@gmail.com>
2016-11-26 22:40:15 +0800

    menu principal
    una propuesta del menu principla, aun me faltan detalles

--- app/src/main/java/com/nicarial/nicarial/MenuPrincipal.java
+++ app/src/main/java/com/nicarial/nicarial/MenuPrincipal.java
@@ -1,9 +1,10 @@
 package com.nicarial.nicarial;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MenuPrincipal extends AppCompatActivity {
@@ -13,24 +14,25 @@ public class MenuPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-        ImageView ivuser = (ImageView) findViewById(R.id.ivuser);
-        ivuser.setOnClickListener(new View.OnClickListener() {
+
+        Button btn_deduc = (Button) findViewById(R.id.btn_deduc);
+        btn_deduc.getBackground().setAlpha(20);
+
+
+
+        Button btn_salir = (Button) findViewById(R.id.btn_salir);
+        btn_salir.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent1 = new Intent(v.getContext(), Usuarios.class);
-                startActivityForResult(intent1, 0);
+                finish();
+
             }
         });
 
-        ImageView ivdonation = (ImageView) findViewById(R.id.ivdonation);
-        ivdonation.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent2 = new Intent(v.getContext(), DonacionesActivity.class);
-                startActivityForResult(intent2, 0);
-            }
-        });
+        Button btn_feed = (Button) findViewById(R.id.btn_feed);
+        btn_feed.getBackground().setAlpha(51);
 
         ImageView ivcalculator = (ImageView) findViewById(R.id.ivcalculator);
-        ivcalculator.setOnClickListener(new View.OnClickListener() {
+        btn_deduc.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent3 = new Intent(v.getContext(), Ingresos.class);
                 startActivityForResult(intent3, 0);
@@ -38,12 +40,14 @@ public class MenuPrincipal extends AppCompatActivity {
         });
 
         ImageView ivfeedback = (ImageView) findViewById(R.id.ivfeedback);
-        ivfeedback.setOnClickListener(new View.OnClickListener() {
+        btn_feed.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent4 = new Intent(v.getContext(), FeedbackActivity.class);
                 startActivityForResult(intent4, 0);
             }
         });
+
+
 
     }
 }
