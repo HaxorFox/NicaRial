53817dc0628d3ea750f68d7d69c455caab5b3168
draenwing <draenwing@gmail.com>
2016-11-27 09:38:00 +0800

    Base de datos en feedback
    Se agrego la base de datos para guardar los comentarios

--- app/src/main/java/com/nicarial/nicarial/Contract.java
+++ app/src/main/java/com/nicarial/nicarial/Contract.java
@@ -12,6 +12,48 @@ public class Contract {
 
     public Contract(){}
 
+    public Contract(int id, String name, String _comentario){
+        this._id = id;
+        this._nombre = name;
+        this._comentario= _comentario;
+    }
+    public Contract( String name, String _comentario){
+
+        this._nombre = name;
+        this._comentario= _comentario;
+    }
+
+
+
+    // getting ID
+    public int getID(){
+        return this._id;
+    }
+
+    // setting id
+    public void setID(int id){
+        this._id = id;
+    }
+
+    // getting name
+    public String getName(){
+        return this._nombre;
+    }
+
+    // setting name
+    public void setName(String name){
+        this._nombre = name;
+    }
+
+    // getting phone number
+    public String getComment(){
+        return this._comentario;
+    }
+
+    // setting phone number
+    public void setComment(String phone_number){
+        this._comentario = phone_number;
+    }
 
 
 }
--- app/src/main/java/com/nicarial/nicarial/FeedbackActivity.java
+++ app/src/main/java/com/nicarial/nicarial/FeedbackActivity.java
@@ -1,13 +1,34 @@
 package com.nicarial.nicarial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FeedbackActivity extends AppCompatActivity {
-
+    DataBaseHandler db = new DataBaseHandler(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        final EditText txt_nombre = (EditText) findViewById(R.id.txt_nombre);
+        final EditText txt_comment = (EditText) findViewById(R.id.txt_comment);
+
+        Button btn_enviar = (Button) findViewById(R.id.btn_submit);
+
+
+
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            db.addContract(new Contract(txt_nombre.getText().toString(),txt_comment.getText().toString()));
+                Toast.makeText(FeedbackActivity.this, "Comentario Guardado",
+                        Toast.LENGTH_LONG).show();
+                txt_nombre.setText("");
+                txt_comment.setText("");
+            }
+        });
     }
 }
